52dc7217c6fed3375ae00571ba9b61fecab73595
Valerio Galano <v.galano@daredevel.com>
2024-05-11 12:42:14 +0200

    Improve convert script

--- utils/convert_chapters.php
+++ utils/convert_chapters.php
@@ -1,48 +1,44 @@
 <?php
-function convertFile($filename)
+function convert_file($input_filename, $output_filename)
 {
-    $lines = file($filename, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    $lines = file($input_filename, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
     // Initialize variables for chapters
     $chapters = array();
 
     foreach ($lines as $line) {
-        // Ignore empty lines
-        if (trim($line) == '') continue;
 
         // Extract title and time
-        preg_match_all('/(\d+\.\d+)\s+(\d+\.\d+)\s+([^\d]+)/', $line, $matches, PREG_SET_ORDER);
+        preg_match_all('/(\d+\.\d+)\s+(\d+\.\d+)\s+(\D+)/', $line, $matches, PREG_SET_ORDER);
 
         foreach ($matches as $match) {
-            $startTime = floatval($match[1]);
-            $title = trim($match[3]);
 
             // Add the chapter
             $chapters[] = array(
-                'startTime' => $startTime,
-                'title' => $title
+                'startTime' => floatval($match[1]),
+                'title' => trim($match[3])
             );
         }
     }
 
-    $json = json_encode([
+    // Write the JSON data to a new file
+    file_put_contents($output_filename, json_encode([
         "version" => "1.2.0",
         "chapters" => $chapters
-    ]);
-
-    // Output filename
-    $outputFilename = dirname(__FILE__).'/../public/chapters/' . pathinfo($filename, PATHINFO_FILENAME) . '.json';
-
-    // Write the JSON data to a new file
-    file_put_contents($outputFilename, $json);
+    ]));
 }
 
+$path = dirname(__FILE__) . '/../public/chapters/';
+
 // Convert all files in the public/chapters directory
-$files = glob(dirname(__FILE__).'/../public/chapters/*.txt');
+$files = glob($path .'*.txt');
 foreach ($files as $file) {
 
+    // Output filename
+    $outputFilename = $path . pathinfo($file, PATHINFO_FILENAME) . '.json';
+
     // If output file exists, skip
-    if (file_exists(dirname(__FILE__).'/../public/chapters/' . pathinfo($file, PATHINFO_FILENAME) . '.json')) {
+    if (file_exists($outputFilename)) {
         echo "Skipping $file\n";
         continue;
     }
@@ -50,5 +46,5 @@ foreach ($files as $file) {
     echo "Converting $file\n";
 
     // Convert the file
-    convertFile($file);
+    convert_file($file, $outputFilename);
 }
